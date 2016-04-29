a4f1c5a6aee1508ab44be477cad2c79b7480dc32
Yang Lin <todoubaba@gmail.com>
2016-04-30 05:37:03 +0800

    Upgrade to grails 3.1.6 (#7)

--- tm-uaa/src/main/groovy/cn/edu/bnuz/bell/tm/uaa/TmUserDetailsService.groovy
+++ tm-uaa/src/main/groovy/cn/edu/bnuz/bell/tm/uaa/TmUserDetailsService.groovy
@@ -3,7 +3,6 @@ package cn.edu.bnuz.bell.tm.uaa
 import cn.edu.bnuz.bell.security.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
